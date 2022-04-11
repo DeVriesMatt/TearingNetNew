51d8b16e1add6b62acac5e1296db5ed267437d2f
DeVriesMatt <matthew.devries.13@gmail.com>
2022-04-11 20:40:15 +0100

    update to train on vae

--- main_vae.py
+++ main_vae.py
@@ -1,11 +1,39 @@
 import torch
 from torch.utils.data import DataLoader
-from training_functions import train
+from training_functions import train_vae
 from encoders.dgcnn import ChamferLoss
 from dataset import PointCloudDatasetAllBoth
 from autoencoder import GraphAutoEncoder
-from chamfer import ChamferLoss1
+from torch import nn
 import argparse
+
+
+class VAE(nn.Module):
+    def __init__(self, encoder, decoder, encoder_type, decoder_type, latent_dim=8):
+        super(VAE, self).__init__()
+        self.encoder = encoder
+        self.latent_dim = latent_dim
+        self.enc_mu = nn.Linear(50, self.latent_dim)
+        self.enc_log_var = nn.Linear(50, self.latent_dim)
+        self.fc_dec = nn.Linear(self.latent_dim, 50)
+        self.decoder = decoder
+        self.encoder_type = encoder_type
+        self.decoder_type = decoder_type + "vae"
+
+    def reparametrize(self, mus, log_vars):
+        sigma = torch.exp(0.5*log_vars)
+        z = torch.randn(size=(mus.size(0), mus.size(1)))
+        z = z.type_as(mus)
+        return mus + sigma * z
+
+    def forward(self, x):
+        embeddings = self.encoder(x)
+        mu = self.enc_mu(embeddings)
+        log_var = self.enc_log_var(embeddings)
+        z = self.reparametrize(mu, log_var)
+        upsamples = self.fc_dec(z)
+        outs, grid = self.decoder(upsamples)
+        return outs, mu, log_var, embeddings, z
 
 
 if __name__ == "__main__":
@@ -40,9 +68,24 @@ if __name__ == "__main__":
     batch_size = 16
     learning_rate = 0.00000001
 
-    model = GraphAutoEncoder(num_features=50, k=20, encoder_type="dgcnn", decoder_type='foldingnet')
-    model.load_state_dict(checkpoint['model_state_dict'])
+    ae = GraphAutoEncoder(num_features=50, k=20, encoder_type="dgcnn", decoder_type='foldingnet')
+    model = VAE(encoder=ae.encoder,
+                decoder=ae.decoder,
+                encoder_type=ae.encoder_type,
+                decoder_type=ae.decoder_type)
+    # model.load_state_dict(checkpoint['model_state_dict'])
     print(checkpoint['loss'])
+
+    model_dict = model.state_dict()  # load parameters from pre-trained FoldingNet
+    for k in checkpoint['model_state_dict']:
+        if k in model_dict:
+            model_dict[k] = checkpoint['model_state_dict'][k]
+            print("    Found weight: " + k)
+        elif k.replace('folding1', 'folding') in model_dict:
+            model_dict[k.replace('folding1', 'folding')] = checkpoint['model_state_dict'][k]
+            print("    Found weight: " + k)
+    model.load_state_dict(model_dict)
+
     dataset = PointCloudDatasetAllBoth(df, root_dir)
 
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -56,4 +99,4 @@ if __name__ == "__main__":
         weight_decay=1e-6,
     )
 
-    train(model, dataloader, num_epochs, criterion, optimizer, output_path)
+    train_vae(model, dataloader, num_epochs, criterion, optimizer, output_path)
--- training_functions.py
+++ training_functions.py
@@ -5,6 +5,10 @@ from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 from reporting import get_experiment_name
 
+def latent_loss(mu, log_var):
+    kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0)
+    return kld_loss
+
 
 def train(model, dataloader, num_epochs, criterion, optimizer, output_dir):
     name_logging, name_model, name_writer, name = get_experiment_name(
@@ -56,6 +60,75 @@ def train(model, dataloader, num_epochs, criterion, optimizer, output_dir):
                         f"LossTot: {batch_loss}"
                     )
 
+            total_loss = running_loss / len(dataloader)
+            if total_loss < best_loss:
+                checkpoint = {
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "epoch": epoch,
+                    "loss": total_loss,
+                }
+                best_loss = total_loss
+                torch.save(checkpoint, name_model)
+                logging.info(f"Saving model to {name_model} with loss = {best_loss}.")
+
+
+def train_vae(model, dataloader, num_epochs, criterion, optimizer, output_dir):
+
+    name_logging, name_model, name_writer, name = get_experiment_name(
+        model=model, output_dir=output_dir
+    )
+
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    logging.basicConfig(filename=name_logging, level=logging.INFO)
+    logging.info(f"Started training model {name} at {now}.")
+    writer = SummaryWriter(log_dir=name_writer)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.train()
+    best_loss = 1000000000
+    niter = 1
+    for epoch in range(num_epochs):
+        batch_num = 1
+        running_loss = 0.0
+        model.train()
+        with tqdm(dataloader, unit="batch") as tepoch:
+            for data in tepoch:
+                tepoch.set_description(f"Epoch {epoch}")
+
+                inputs = data[0]
+                inputs = inputs.to(device)
+                batch_size = inputs.shape[0]
+
+                # ===================forward=====================
+                with torch.set_grad_enabled(True):
+                    output, mu, log_var, features, z = model(inputs)
+                    optimizer.zero_grad()
+                    loss_rec = criterion(inputs, output)
+                    loss_kl = latent_loss(mu, log_var)
+                    # ===================backward====================
+                    loss = loss_rec + loss_kl
+                    loss.backward()
+                    optimizer.step()
+
+                batch_loss = loss.detach().item() / batch_size
+                batch_loss_rec = loss_rec.detach().item() / batch_size
+                batch_loss_kl = loss_kl.detach().item() / batch_size
+                running_loss += batch_loss
+                batch_num += 1
+                writer.add_scalar("/Loss", batch_loss, niter)
+                niter += 1
+                tepoch.set_postfix(loss=batch_loss)
+
+                if batch_num % 10 == 0:
+                    logging.info(
+                        f"[{epoch}/{num_epochs}]"
+                        f"[{batch_num}/{len(dataloader)}]"
+                        f"LossTot: {batch_loss}"
+                        f"Loss Rec: {batch_loss_rec}"
+                        f"Loss kl: {batch_loss_kl}"
+                    )
             total_loss = running_loss / len(dataloader)
             if total_loss < best_loss:
                 checkpoint = {
